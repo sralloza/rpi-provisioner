69c0dfa69c59fa4193adb5463703e4b4369f3c1b
Diego Alloza González <sralloza@gmail.com>
2021-06-24 17:21:17 +0200

    chore: improve docker test script

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,3 @@
 docker build -t rpi-simulator .
-docker run -d --rm -p 22:22 rpi-simulator
+docker run -d --rm --name rpi-simulator -p 22:22 rpi-simulator
+docker stop rpi-simulator && docker run -d --rm --name rpi-simulator -p 22:22 rpi-simulator
