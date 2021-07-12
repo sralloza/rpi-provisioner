a18dbdc45b020be70adfd181067244cc48df78f8
Diego Alloza <diego.allozagonzalez.practicas@telefonica.com>
2021-07-12 13:20:11 +0200

    chore: set up test script

--- test_deploy.py
+++ test_deploy.py
@@ -3,5 +3,6 @@ from time import sleep
 
 
 system("docker stop rpi-simulator")
-sleep(1)
+sleep(3)
 system("docker run -d --rm --name rpi-simulator -p 22:22 rpi-simulator")
+system("python main.py")
